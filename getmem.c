4b916cb887c10010fa386cbb2418a56d08a459b3
ycarissan <ycarissan@gmail.com>
2021-11-30 12:43:03 +0100

    add argument reader

--- getmem.c
+++ getmem.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
 
 /* msleep(): Sleep for the requested number of milliseconds. */
 int msleep(long msec)
@@ -25,19 +27,43 @@ int msleep(long msec)
     return res;
 }
 
+int main(int argc, char **argv) {
+	char *cvalue = NULL;
+	int index;
+	int c;
+	unsigned long long int fact=1000;
+	unsigned long long int val=1000;
 
-int main() {
+	while ((c = getopt (argc, argv, "mgMG")) != -1)
+		switch (c)
+		{
+			case 'm':
+			case 'M':
+				printf("mega");
+				fact = 1;
+				break;
+			case 'g':
+			case 'G':
+				printf("giga");
+				fact=1000;
+				break;
+			default:
+				abort ();
+		}
+	for (index = optind; index < argc; index++)
+		val = (unsigned long long int)argv[index];
+		printf ("Valeur lue :%llu\n", val);
 
+	unsigned long long int nelem = fact*val;
+	printf("Taille de l'allocation en octets :%llu\n",nelem);
 	printf("Debut de l'allocation\n");
-	long long int ngiga = 4;
-	long long int nelem = ngiga*1000000000;
 	char *array = malloc(nelem);
 	if (array == NULL) {
 		fprintf(stderr, "malloc failed\n");
 		return -1;
 	}
 	printf("Fin de l'allocation\n");
-        for (long long int i=0; i<nelem; i++){
+        for (unsigned long long int i=0; i<nelem; i++){
                 array[i] = 'A';
         }
 
